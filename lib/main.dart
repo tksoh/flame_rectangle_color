a4870a783441106d67795f6809fa53f299eea6ba
TK Soh <teekaysoh@gmail.com>
2023-02-17 10:17:16 +0800

    add fading marker at touch position

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,32 @@ import 'package:flutter/material.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
+
+class PositionMarker extends CircleComponent {
+  double? lifeSpan;
+
+  PositionMarker({
+    Vector2? position,
+    double radius = 5.0,
+    color = Colors.red,
+    this.lifeSpan,
+  }) : super(
+          position: position,
+          radius: radius,
+          anchor: Anchor.center,
+        ) {
+    setColor(color);
+  }
+
+  @override
+  Future<void> onLoad() {
+    if (lifeSpan != null) {
+      add(RemoveEffect(delay: lifeSpan!));
+    }
+    return super.onLoad();
+  }
+}
 
 class Shield extends RectangleComponent with Tappable, HasGameRef<MyGame> {
   Shield();
@@ -18,6 +44,12 @@ class Shield extends RectangleComponent with Tappable, HasGameRef<MyGame> {
   @override
   bool onTapDown(TapDownInfo info) {
     debugPrint('Sheild: onTapDown');
+    final marker = PositionMarker(
+      position: info.eventPosition.game,
+      radius: 10,
+      lifeSpan: 2,
+    );
+    gameRef.add(marker);
     return false; // stop tap event propagation
   }
 
